70cee18dfc3f2c6a9e691ccd43490e750029da18
Mukeshkumar16 <87300956+Mukeshkumar16@users.noreply.github.com>
2021-12-21 18:36:14 +0530

    Add files via upload

---
+++ DeleteContact.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.*;
+import java.io.*;
+import org.json.JSONObject;
+import org.json.JSONException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletContext;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Map;
+import com.google.gson.*;
+import java.util.ListIterator;
+@WebServlet("/DeleteContact")
+public class DeleteContact extends HttpServlet{
+    private static final long serialVersionVID=1L;
+      public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        PrintWriter out=response.getWriter();
+           ServletContext context=getServletContext();
+           String ContactName=(String)context.getAttribute("ViewContact");
+           String finalLocation=(String)context.getAttribute("file");
+           File file=new File(finalLocation);
+           FileInputStream fstream=new FileInputStream(file);
+           BufferedReader br=new BufferedReader(new InputStreamReader(fstream));
+           StringBuffer buffer = new StringBuffer();
+           String str;
+           String oldLine ="";
+           while ((str=br.readLine())!=null) {
+            String name="";
+            name+=str.substring(str.lastIndexOf(":")+1,str.length());
+         if(name.equals(ContactName)){
+             oldLine+=str;
+             String st;
+             while((st=br.readLine())!=null){
+                if(st.equals("END:VCARD")){
+                      break;
+                  }else{
+                     continue;
+                   }
+             } 
+          }else{
+             buffer.append(str+System.lineSeparator());
+             }
+        
+      }
+      String fileContents = buffer.toString();
+      FileWriter writer = new FileWriter(finalLocation);
+      writer.append(fileContents);
+      writer.flush();
+      response.sendRedirect("redirect.html");
+           
+         }
+        
+}
